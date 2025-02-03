206deb8a800510def30ef98bc082f886c89fbd16
fector101 <fabianjoseph063@gmail.com>
2025-02-03 01:18:41 +0100

    faster debugging

--- build patches/start.sh
+++ build patches/start.sh
@@ -1,3 +1,6 @@
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+
+# For Debugging
+buildozer android debug && adb install bin/lan_ft-1.0-arm64-v8a_armeabi-v7a-debug.apk && adb shell monkey -p org.laner.lan_ft 1 && adb logcat | grep python
