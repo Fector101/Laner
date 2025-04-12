f4a3f79985ae78abc431fdf23a34bdf32040ec82
fector101 <fabianjoseph063@gmail.com>
2025-04-12 21:14:32 +0100

    bad types

--- utils/typing/main.py
+++ utils/typing/main.py
@@ -1,10 +1,11 @@
 # Werid typing for autocomplete
 from components import BottomNavigationBar
 from components.templates import MyBtmSheet
+from utils import Settings
 
 
 # Avoiding Circular Import Error
 class Laner:
     bottom_navigation_bar=BottomNavigationBar
     btm_sheet = MyBtmSheet
-    # settings = Settings()
+    settings = Settings
