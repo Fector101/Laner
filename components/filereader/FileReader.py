3871afdfd503c1223873cc6d5cbcab9acc01d7df
fector101 <fabianjoseph063@gmail.com>
2025-04-18 23:17:03 +0100

    new folder for filereader to keep things organizied better looking file reader + themeing

--- components/filereader/FileReader.py
+++ components/filereader/FileReader.py
@@ -1,48 +1,74 @@
 import os
-
 from kivy.core.clipboard import Clipboard
 from kivy.core.window import Window
 from kivy.utils import platform
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.uix.textinput import TextInput
+from kivymd.app import MDApp
 
 if platform == 'android':
     from kivymd.toast import toast
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivy.uix.textinput import TextInput
 
 from components import HeaderBasic
 from components.popup import Snackbar
-from utils.helper import makeDownloadFolder, getFileName, urlSafePath
+from utils.helper import getFileName, urlSafePath, getAppFolder
 from utils import AsyncRequest
 
+class CustomTextInput(TextInput):
+    def __init__(self, header_height,**kwargs):
+        super().__init__(**kwargs)
+        self.app = MDApp.get_running_app()
+        self.header_height=header_height
+        self.background_normal = '' # border-color active focus
+        # self.background_active = '' # border-color inactive focus
+        self.background_color = (.1, .1, .1, 1) if self.app.theme_cls.theme_style == 'Dark' else [.9,.9,.9,1]
+        self.foreground_color=[1,1,1,1] if self.app.theme_cls.theme_style == 'Dark' else (.1, .1, .1, 1)
+        self.multiline = True
+        self.size_hint_y = None
+        self.padding=10
+        self.height = Window.height - self.header_height
+        Window.bind(size=self.on_resize_box)
+        self.cursor_color = self.app.theme_cls.primaryColor
 
+        # Changes|Add more than border color change text color
+        # with self.canvas.before:
+            # self.border_color = Color(1, 0, .1, 1)  # changes textcolor for some reason
+        #     self.border_rect = Line(rectangle=(self.x, self.y, self.width, self.height), width=1.5)
+        # self.bind(pos=self.update_border, size=self.update_border)
+
+    def on_resize_box(self, window_object, window_size):
+        self.height = window_size[1] - self.header_height
+
+    # def update_border(self, *args):
+    #     self.border_rect.rectangle = (self.x, self.y, self.width, self.height)
 class FileReader(MDBoxLayout):
     def __init__(self, file_path:str, close_btn_callback, **kwargs):
         super().__init__(**kwargs)
-        self.file_path =file_path
-        self.my_downloads_folder = makeDownloadFolder()
+        self.file_path = file_path
+        self.save_folder = os.path.join(getAppFolder(),'.filereader')
+        if not os.path.exists(self.save_folder):
+            os.mkdir(self.save_folder)
         self.orientation='vertical'
         self.close_btn_callback = close_btn_callback
-        self.md_bg_color = [0, 0, 0, 1]
         btns_data=[
-            {'icon':'content-copy','function':self.copy_all_content},
             {'icon':'content-paste','function':self.paste_content},
-            {'icon':'select-all','function':self.select_all_content},
+            {'icon': 'content-copy', 'function': self.copy_all_content},
+            # {'icon':'select-all','function':self.select_all_content},
             {'icon':'upload','function':self.upload_file}
         ]
-        self.header = HeaderBasic(text=self.file_path,btns=btns_data,back_btn_func=self.close)
-        self.text_box= TextInput(text='Hello world', multiline=True,size_hint_y=None,height=Window.height-self.header.height)
-        Window.bind(size=self.on_resize_box)
+        self.header = HeaderBasic(text=self.file_path,btns=btns_data,back_btn_func=self.close,height=80)
+        self.text_box= CustomTextInput(text='Loading...',header_height=self.header.height)
         self.add_widget(self.header)
         self.add_widget(self.text_box)
-        self.download_content()
+        self.__download_content()
         # self.dropDown = MDDropdownMenu()
-    def download_content(self):
+
+    def __download_content(self):
         file_name = getFileName(self.file_path)
-        save_path=urlSafePath(os.path.join(self.my_downloads_folder,file_name))
-        AsyncRequest().download_file(file_path=self.file_path,save_path=save_path,success=self.read_content,failed=self.failed_to_download)
+        save_path=os.path.join(self.save_folder, file_name)
+        AsyncRequest(notifications=False).download_file(file_path=urlSafePath(self.file_path),save_path=save_path,success=self.read_content,failed=self.failed_to_download)
 
     def read_content(self, saved_path):
-        # print(saved_path)
         with open(saved_path, "r", encoding="utf-8") as f:
             content = f.read()
         self.text_box.text = content
@@ -62,17 +88,18 @@ class FileReader(MDBoxLayout):
             f.write(self.text_box.text)
         folder_path =  os.path.dirname(self.file_path)
 
-        AsyncRequest().upload_file(file_path=file_path,save_path=folder_path,success=self.success_upload)
+        AsyncRequest(notifications=False).upload_file(file_path=file_path,save_path=folder_path,success=self.success_upload)
+
     def success_upload(self):
         self.toast_msg('Uploaded Successfully')
-    def on_resize_box(self,window_object,window_size):
-        self.text_box.height=window_size[1]-self.header.height
+
     @staticmethod
     def toast_msg(text,widget=None):
         if platform == 'android':
             toast(text)
         else:
             Snackbar(h1=text)
+
     def copy_all_content(self,widget=None):
         Clipboard.copy(self.text_box.text)
         self.toast_msg('File Copied')
@@ -82,15 +109,15 @@ class FileReader(MDBoxLayout):
         self.text_box.text = copy_board_content
         self.text_box.cancel_selection()
 
-    def select_all_content(self,widget=None):
-        self.text_box.select_all()
-
     def reset_transition_duration(self):
         self.swiper.transition_duration = 0.2
 
     def close(self, widget=None):
         self.parent.remove_widget(self)
         self.close_btn_callback()
+
+    # def select_all_content(self,widget=None):
+    #     self.text_box.select_all()
 
     # def save_file(self,widget=None):
     #     pass
